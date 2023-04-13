c6e2b5255d8694f41de8549cbd2cbc94e122cd41
bsz90 <bsz90@users.noreply.github.com>
2023-04-13 17:49:28 -0400

    Updated types to refine holidays

--- src/pages/types.tsx
+++ src/pages/types.tsx
@@ -1,13 +1,28 @@
-export type DynamicHoliday = {
-  dateRange: [number, number];
-  week?: number;
-  occurence?: number | [number, number];
-  day: number;
-  name: string;
-  specialConditions?: (counter: Counter, j: number) => boolean;
+import { RelationalOperatorOrHigher } from "typescript";
+
+export type Holiday = DynamicDay | StaticDay;
+
+export type ReligiousHolidays = (year: number) => Date;
+
+export type Holidays = {
+  [key: string]: Date;
 };
 
-export type StaticHolidays = {
+export type Breaks = {
+  [key: string]: Date[];
+};
+
+export type DynamicDay = {
+  dateRange: [number, number];
+  week?: number;
+  occurence?: number | number[];
+  month: number;
+  day: number;
+  name: string;
+  determineDate?: (year: number) => Date;
+};
+
+export type StaticDay = {
   month: number;
   date: number;
   name: string;
@@ -24,4 +39,24 @@ export type Counter = {
   letterDays: number;
   occurence: number;
   occurenceCounter: number;
+};
+
+export type Settings = {
+  schoolYear: number[];
+  firstDay: Date;
+  totalDays: number;
+  daysInCycle: number;
+  classPeriodsPerDay: number;
+  holidays: Holidays;
+  breaks: Breaks;
+  proDevDays: Date[];
+  customDays: Date[];
+};
+
+export type SettingKey = keyof Settings;
+
+export type Period = {
+  name: string;
+  time: number;
+  type: "class" | "free" | "meeting" | "lunch";
 };
