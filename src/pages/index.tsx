796d65ae85e11ba87c489fafeb251986b285caa0
bsz90 <bsz90@users.noreply.github.com>
2023-04-24 19:33:44 -0400

    Add settings state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+import { AppSettings } from "./AppSettings";
 import { Calendar } from "./Calendar";
+import { defaultSettings } from "./constants";
 
 export default function Home() {
+  const [settings, setSettings] = useState(defaultSettings);
   return (
     <main className="flex min-h-screen flex-col items-center bg-white justify-center p-20 overflow-hidden">
+      <AppSettings settings={settings} setSettings={setSettings} />
       <Calendar />
     </main>
   );
